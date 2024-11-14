89206f793f78ca37a7d4eb199af78e2b1bcc83c2
sneeppy <vinidiktovp@gmail.com>
2024-11-14 21:47:31 +0300

    updating the build file.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("org.jetbrains.dokka") version "1.9.20"
+}
+
+tasks.dokkaHtml {
+    outputDirectory.set(file("C:\\Users\\Павел\\IdeaProjects\\Amaru\\build\\dokka\\html"))
+
+    dokkaSourceSets {
+        configureEach {
+            // Пропускать элементы без документации
+            reportUndocumented = true
+            skipDeprecated = true
+        }
+    }
 }
 
 group = "org.example"
@@ -11,6 +24,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.mockito:mockito-core:4.3.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+
+    implementation("org.telegram:telegrambots:6.3.0")
+    implementation("org.telegram:telegrambotsextensions:6.1.0")
 }
 
 tasks.test {
